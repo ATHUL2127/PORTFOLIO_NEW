4ec8eb2268ec6340f283ce5a80dbd21b5f4baccc
ATHUL2127 <athulparayil2002@gmail.com>
2024-10-20 16:05:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();  // Prevent actual form submission
+    document.getElementById('thankYouMessage').style.display = 'block';
+});
